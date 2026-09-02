9f5cf6c750196badb4e70f13e7ef434caf537513
Ashwot-Acharya <ashwot1133@gmail.com>
2026-09-03 01:15:15 +0545

    stopping the Vissualizer

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { visualizer } from 'rollup-plugin-visualizer'
 
+const isReport = process.env.REPORT === 'true'
+
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({
-      open: false,
-      gzipSize: true,
-      brotliSize: true,
-    }),
+    ...(isReport ? [visualizer({ open: false, gzipSize: true, brotliSize: true })] : []),
   ],
   base: "/"
 })
